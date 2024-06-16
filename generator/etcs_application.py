9100a3a6fdfdd86556c8e861e809313f12dbca30
Lukas Rost <git@lukasrost.de>
2024-06-16 17:04:47 +0200

    implement MA and generator driver script

--- generator/etcs_application.py
+++ generator/etcs_application.py
@@ -1,15 +1,80 @@
 import bitstruct
 
 
-class M3_MovementAuthority:
-    nid_message = 3
+# TODO for forging MAs (later), we probably want to have the packets as separate classes
 
-    # NID_MESSAGE = 8 bits unsigned
-    # L_MESSAGE = 10 bits unsigned (length of complete message including padding in bytes)
-    # T_TRAIN = 32 bits unsigned (train clock value with resolution of 10 ms)
-    # M_ACK = 1 bit (acknowledgement required or not)
-    # NID_LRBG = NID_C (10 bit country identity) + NID_BG (14 bit balise identity number)
-    # Packet 15 (see PDF)
+
+class M3_MovementAuthority:
+    nid_message = 3  # 8 bit (message type identifier)
+    l_message = 24  # 10 bit (length of complete message including padding in bytes)
+    t_train: int  # 32 bit (train clock value)
+    m_ack: bool  # 1 bit (acknowledgement required or not)
+    nid_c: int  # 10 bit (country identity)
+    nid_bg: int  # 14 bit (balise identity number of LRBG)
+    # Packet 15
+    nid_packet = 15  # 8 bit (message type identifier)
+    q_dir = 2  # 2 bit (validity direction of MA relative to LRBG, 2 = both directions)
+    l_packet = 113  # 13 bit (length of packet in bits)
+    q_scale = 1  # 2 bit (1 = 1m scale)
+    v_ema: int  # 7 bit (permitted speed at end of MA in 5 km/h steps)
+    t_ema = 1023  # 10 bit (validity period of v_ema, 1023 = infinite)
+    n_iter = 0  # 5 bit (number of non-end track sections in the MA)
+    l_endsection: int  # 15 bit (length of end section in MA)
+    q_sectiontimer = False  # 1 bit (whether packet contains section time-out)
+    q_endtimer = False  # 1 bit (whether packet contains end section time-out)
+    q_dangerpoint = False  # 1 bit (whether packet contains danger point info)
+    q_overlap = True  # 1 bit (whether packet contains overlap info)
+    d_startol: int  # 15 bit (distance from overlap timer start to end of MA)
+    t_ol = 1023  # 10 bit (validity period for overlap, 1023 = infinite)
+    d_ol: int  # 15 bit (distance from end of MA to end of overlap)
+    v_releaseol = 126  # 7 bit (release speed associated with overlap in 5 km/h steps, 126 = use onboard calculated release speed)
+
+    def __init__(
+        self,
+        t_train: int,
+        m_ack: bool,
+        nid_c: int,
+        nid_bg: int,
+        v_ema: int,
+        l_endsection: int,
+        d_startol: int,
+        d_ol: int,
+    ) -> None:
+        self.t_train = t_train
+        self.m_ack = m_ack
+        self.nid_c = nid_c
+        self.nid_bg = nid_bg
+        self.v_ema = v_ema
+        self.l_endsection = l_endsection
+        self.d_startol = d_startol
+        self.d_ol = d_ol
+
+    def get_bytes(self) -> bytes:
+        return bitstruct.pack(
+            "u8 u10 u32 b1 u10 u14 u8 u2 u13 u2 u7 u10 u5 u15 b1 b1 b1 b1 u15 u10 u15 u7",
+            self.nid_message,
+            self.l_message,
+            self.t_train,
+            self.m_ack,
+            self.nid_c,
+            self.nid_bg,
+            self.nid_packet,
+            self.q_dir,
+            self.l_packet,
+            self.q_scale,
+            self.v_ema,
+            self.t_ema,
+            self.n_iter,
+            self.l_endsection,
+            self.q_sectiontimer,
+            self.q_endtimer,
+            self.q_dangerpoint,
+            self.q_overlap,
+            self.d_startol,
+            self.t_ol,
+            self.d_ol,
+            self.v_releaseol,
+        )
 
 
 class M16_UnconditionalEmergencyStop:
@@ -48,6 +113,7 @@ class M136_TrainPositionReport:
     l_message = 23  # 10 bit (length of complete message including padding in bytes)
     t_train: int  # 32 bit (train clock value)
     nid_engine: int  # 24 bit (unique ETCS identity number)
+    # Packet 0
     nid_packet = 0  # 8 bit (packet type identifier)
     l_packet = 108  # 13 bit (length of packet in bits)
     q_scale = 1  # 2 bit (1 = 1m scale)
--- generator/generator.py
+++ generator/generator.py
@@ -1,10 +1,101 @@
-from bitstring import BitArray
-from etcs_application import M16_UnconditionalEmergencyStop
+import random
+import os
+from etcs_application import (
+    M3_MovementAuthority,
+    M16_UnconditionalEmergencyStop,
+    M136_TrainPositionReport,
+)
+from euroradio import EuroRadioPacket
 
-# used for testing purposes at the moment
+
+# configuration options
+output_directory = "generated_packets"
+messages_per_type = 100
+seed = 7876553
+
+
+def generate_movement_authority_messages(
+    key: bytes, nid_c: int, count: int
+) -> list[bytes]:
+    messages = []
+    for _ in range(count):
+        t_train = random.getrandbits(32)
+        m_ack = random.getrandbits(1) == 1
+        nid_bg = random.getrandbits(14)
+        v_ema = random.getrandbits(7)
+        l_endsection = random.getrandbits(15)
+        d_startol = random.getrandbits(15)
+        d_ol = random.getrandbits(15)
+        packet = M3_MovementAuthority(
+            t_train, m_ack, nid_c, nid_bg, v_ema, l_endsection, d_startol, d_ol
+        ).get_bytes()
+        er_packet = EuroRadioPacket(packet, key).get_bytes()
+        messages.append(er_packet)
+    return messages
+
+
+def generate_emergency_stop_messages(key: bytes, nid_c: int, count: int) -> list[bytes]:
+    messages = []
+    for _ in range(count):
+        t_train = random.getrandbits(32)
+        m_ack = random.getrandbits(1) == 1
+        nid_bg = random.getrandbits(14)
+        nid_em = random.getrandbits(4)
+        packet = M16_UnconditionalEmergencyStop(
+            t_train, m_ack, nid_c, nid_bg, nid_em
+        ).get_bytes()
+        er_packet = EuroRadioPacket(packet, key).get_bytes()
+        messages.append(er_packet)
+    return messages
+
+
+def generate_position_report_messages(
+    key: bytes, nid_c: int, count: int
+) -> list[bytes]:
+    messages = []
+    # consistency: should not change between messages
+    nid_engine = random.getrandbits(24)
+    for _ in range(count):
+        t_train = random.getrandbits(32)
+        nid_bg = random.getrandbits(14)
+        d_lrbg = random.getrandbits(15)
+        l_doubtover = random.getrandbits(15)
+        l_doubtunder = random.getrandbits(15)
+        packet = M136_TrainPositionReport(
+            t_train, nid_engine, nid_c, nid_bg, d_lrbg, l_doubtover, l_doubtunder
+        ).get_bytes()
+        er_packet = EuroRadioPacket(packet, key).get_bytes()
+        messages.append(er_packet)
+    return messages
+
+
+def output_packets(packets: list[bytes], directory: str, prefix: str):
+    for i, packet in enumerate(packets):
+        with open(f"{directory}/{prefix}_{i}.hex", "w") as file:
+            file.write(
+                packet.hex(" ")
+            )  # this can easily be reversed using bytes.fromhex()
+
+
+def main():
+    os.makedirs(output_directory, exist_ok=True)
+    random.seed(seed)
+    euroradio_key = random.randbytes(24)
+    nid_c = random.getrandbits(10)
+    ma_messages = generate_movement_authority_messages(
+        euroradio_key, nid_c, messages_per_type
+    )
+    es_messages = generate_emergency_stop_messages(
+        euroradio_key, nid_c, messages_per_type
+    )
+    pr_messages = generate_position_report_messages(
+        euroradio_key, nid_c, messages_per_type
+    )
+    output_packets(ma_messages, output_directory, "ma")
+    output_packets(es_messages, output_directory, "es")
+    output_packets(pr_messages, output_directory, "pr")
+    output_packets([euroradio_key], output_directory, "euroradio_key")
+
 
 if __name__ == "__main__":
-    msg = M16_UnconditionalEmergencyStop(8, True, 16, 32, 4)
-    msg_bytes = msg.get_bytes()
-    msg_bin = BitArray(msg_bytes).bin
-    print(" ".join([msg_bin[i : i + 8] for i in range(0, len(msg_bin), 8)]))
+    main()
